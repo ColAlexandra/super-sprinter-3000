0e52c7873bca705e906e4752069b70c0756e8d2a
moveSecondSand <aleksandra.ewa.kolacz@gmail.com>
2019-11-14 07:39:15 +0100

    new

--- data_handler.py
+++ data_handler.py
@@ -7,4 +7,18 @@ STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
 
 
 def get_all_user_story():
-    return []
+    all_user_story = []
+    try:
+        with open(DATA_FILE_PATH , 'r') as new_file:
+            reader = csv.reader(new_file)
+            import_data = list(reader)
+            import_data.pop(0)
+            for line in import_data:
+                dict_temp = {}
+                for i in range(len(DATA_HEADER)):
+                    dict_temp[DATA_HEADER[i]] = line[i]
+                all_user_story.append(dict_temp)
+    except FileNotFoundError:
+        print('File not found')
+            
+    return all_user_story
--- server.py
+++ server.py
@@ -6,11 +6,22 @@ app = Flask(__name__)
 
 
 @app.route('/')
+def index_page():
+    return render_template('index.html')
+
 @app.route('/list')
 def route_list():
     user_stories = data_handler.get_all_user_story()
-
     return render_template('list.html', user_stories=user_stories)
+
+@app.route('/add')
+def route_add():
+    return render_template('add.html')
+
+@app.route('/update')
+def route_update():
+    return render_template('update.html')
+
 
 if __name__ == '__main__':
     app.run(
